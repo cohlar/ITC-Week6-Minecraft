67a1710840a6ca1fb194fb2412593b2274eae9cf
Larry Cohen <cohlar@gmail.com>
2019-12-02 18:17:50 +0200

    added tile class and instances

--- js/main.js
+++ js/main.js
@@ -10,30 +10,50 @@ window.onload = function () {
         }
     }
 
+    class Tile {
+        constructor(name) {
+            this.name = name;
+            this.imgPath = "img/tiles/" + name + ".png";
+        }
+    }
+
     const Minecraft = {
         html: {
-            gameContainer:  $('#game-container'),
-            toolkit:        $('#toolkit'),
+            gameContainer: $('#game-container'),
+            toolkit: $('#toolkit'),
         },
         tools: {
-            axe:            new Tool("axe", "tree"),
-            pickaxe:        new Tool("pickaxe", "rock"),
-            shovel:         new Tool("shovel", "dirt"),
+            axe: new Tool("axe", "tree"),
+            pickaxe: new Tool("pickaxe", "rock"),
+            shovel: new Tool("shovel", "dirt"),
+        },
+        tiles: {
+            diamond: new Tile("diamond"),
+            dirt: new Tile("dirt"),
+            fence: new Tile("fence"),
+            gate: new Tile("gate"),
+            grass: new Tile("grass"),
+            lava: new Tile("lava"),
+            leaf: new Tile("leaf"),
+            rock: new Tile("rock"),
+            stone: new Tile("stone"),
+            tnt: new Tile("tnt"),
+            tree: new Tile("tree"),
         },
     }
 
     // Initiates the game page
-    Minecraft.init = function() {
+    Minecraft.init = function () {
         this.generateHTML();
     };
 
     // Generate HTML elements
-    Minecraft.generateHTML = function() {
+    Minecraft.generateHTML = function () {
         for (const tool in this.tools) {
             const toolContainer = $("<div />").attr("class", "tool-container");
-            toolContainer.append( $("<img />").attr("src", this.tools[tool].imgPath) );
-            toolContainer.append( $("<p />").html(this.tools[tool].name) );
-            this.html.toolkit.append(toolContainer);    
+            toolContainer.append($("<img />").attr("src", this.tools[tool].imgPath));
+            toolContainer.append($("<p />").html(this.tools[tool].name));
+            this.html.toolkit.append(toolContainer);
         }
     };
 
