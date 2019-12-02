304153c4f4393947c0a7c133c2bded08c222f4b5
Larry Cohen <cohlar@gmail.com>
2019-12-02 17:16:01 +0200

    toolkit

--- js/main.js
+++ js/main.js
@@ -2,7 +2,40 @@
 
 window.onload = function () {
 
-    let Minecraft = {};
+    class Tool {
+        constructor(name, tileType) {
+            this.name = name;
+            this.tileType = tileType;
+            this.imgPath = "img/tools/" + name + ".png";
+        }
+    }
+
+    const Minecraft = {
+        html: {
+            gameContainer:  $('#game-container'),
+            toolkit:        $('#toolkit'),
+        },
+        tools: {
+            axe:            new Tool("axe", "tree"),
+            pickaxe:        new Tool("pickaxe", "rock"),
+            shovel:         new Tool("shovel", "dirt"),
+        },
+    }
+
+    // Initiates the game page
+    Minecraft.init = function() {
+        this.generateHTML();
+    };
+
+    // Generate HTML elements
+    Minecraft.generateHTML = function() {
+        for (const tool in this.tools) {
+            const toolContainer = $("<div />").attr("class", "tool-container");
+            toolContainer.append( $("<img />").attr("src", this.tools[tool].imgPath) );
+            toolContainer.append( $("<p />").html(this.tools[tool].name) );
+            this.html.toolkit.append(toolContainer);    
+        }
+    };
 
     // General functions that may be reused outside this projects
     function build2dArray(numOfRows, numOfCols) {
@@ -14,4 +47,7 @@ window.onload = function () {
         }
         return matrix;
     }
+
+    // And this is where all the magic happens
+    Minecraft.init();
 };
