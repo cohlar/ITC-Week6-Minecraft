1278e79a22837435d0cbd761b04ab5b2aa200480
Ilann0 <ilann.ohayon@gmail.com>
2019-12-06 16:45:42 +0200

    bit of reformating + fixed the tools + other small things EVERYTHING WORKS

--- js/MinecraftLibrary.js
+++ js/MinecraftLibrary.js
@@ -1,9 +1,10 @@
 // Library of classes to build a Minecraft game from scratch
+'use strict';
 
 (function (global) {
 
-    global.createTool = function (name, tileType) {
-        return new Tool(name, tileType);
+    global.createTool = function (name, tileTypes) {
+        return new Tool(name, tileTypes);
     }
 
     global.createTile = function (name) {
@@ -24,19 +25,19 @@
 
     class GameElement {
         constructor(name, type) {
-            this.name = name;
-            this.type = type;
-            this.types = type + 's';
+            this.name          = name;
+            this.type          = type;
+            this.types         = type + 's';
             this.imgFolderName = this.types;
-            this.imgPath = 'img/' + this.imgFolderName + '/' + name + '.png';
+            this.imgPath       = 'img/' + this.imgFolderName + '/' + name + '.png';
             this.cursorImgPath = 'img/cursors/' + name + '.png';
         }
     }
 
     class Tool extends GameElement {
-        constructor(name, tileType) {
+        constructor(name, tileTypes) {
             super(name, 'tool');
-            this.tileType = tileType;
+            this.tileTypes = tileTypes;
         }
     }
 
@@ -50,24 +51,26 @@
         constructor(tools, tiles) {
             this.tools = tools;
             this.tiles = tiles;
-            this.count = {};
+            this.count = new Object();
+
             for (let tileKey in this.tiles) {
                 this.count[tileKey] = 0;
             }
+
             this.activeElement = undefined;
         }
     }
 
     class TileGrid {
         constructor(matrix, callbacksMat, tiles) {
-            this.matrix = matrix;
-            this.callbacks = callbacksMat;
-            this.tiles = tiles;
-            this.numOfRows = matrix.length
-            this.numOfCols = matrix[0].length
-            this.maxFillRow = Math.ceil(this.numOfRows / 2);
-            this.baseRow = 4;
-            this.rowOffset = 2;
+            this.matrix     = matrix;
+            this.callbacks  = callbacksMat;
+            this.tiles      = tiles;
+            this.numOfRows  = matrix.length
+            this.numOfCols  = matrix[0].length
+            this.maxFillRow = Math.ceil(this.numOfRows / 2.5);
+            this.baseRow    = Math.ceil(this.numOfRows / 4) //4;
+            this.rowOffset  = 2;
             this.injectMatrixWithTiles(); // Maybe remove
         }
 
@@ -84,6 +87,18 @@
             this.callbacks[row][col] = func;
         }
 
+        hasTileAbove(row, col) {
+            return !!this.matrix[row + 1][col];
+        }
+
+        hasTileBeneath(row, col) {
+            return !!this.matrix[row - 1][col];
+        }
+
+        withProbaOf(n) {
+            return !!n && Math.random() <= n;
+        }
+
         injectMatrixWithTiles() {
             for (let row = 0; row < this.numOfRows; row++) {
                 for (let col = 0; col < this.numOfCols; col++) {
@@ -94,55 +109,11 @@
             }
             this.polishItUp();
         }
-        // add trees and grass
-        polishItUp() {
-            for (let row = 0; row < this.numOfRows; row++) {
-                if (!!this.matrix[row + 1]) {
-                    for (let col = 0; col < this.numOfCols; col++) {
-
-                        if (!!this.matrix[row][col] &&
-                            !this.hasTileAbove(row, col) &&
-                            this.matrix[row][col].name === 'dirt') {
-
-                            this.matrix[row][col] = this.tiles.grass;
-                        } else if (!!this.matrix[row][col] &&
-                            !this.hasTileAbove(row, col) &&
-                            this.matrix[row][col].name === 'tree') {
-                            this.injectTree(row, col);
-                        }
-                    }
-                }
-            }
-
-        }
-
-        hasTileAbove(row, col) {
-            return !!this.matrix[row + 1][col];
-        }
-
-        hasTileBeneath(row, col) {
-            return !!this.matrix[row - 1][col];
-        }
-
-        injectTree(row, col) {
-            for (let i = 0; i < 3; i++) {
-                // Trunk
-                this.matrix[row + i][col] = this.tiles.tree;
-                // Leaves
-                this.matrix[row + 3 + i][col - 1] = this.tiles.leaf;
-                this.matrix[row + 3 + i][col] = this.tiles.leaf;
-                this.matrix[row + 3 + i][col + 1] = this.tiles.leaf;
-            }
-        }
-
-        withProbaOf(n) {
-            return !!n && Math.random() <= n;
-        }
 
         randomTile(row, col) {
             switch (true) {
                 case row === 0:
-                    return this.tiles.lava;
+                    return (this.withProbaOf(0.98) ? this.tiles.lava : this.tiles.diamond);
 
                 case row < this.baseRow:
                     return this.tiles.dirt;
@@ -155,13 +126,14 @@
                             return this.tiles.dirt;
 
                         } else if (this.withProbaOf(0.3) &&
-                            this.matrix[row - 1][col].name === 'dirt') {
-                            return this.tiles.rock;
+                                   this.matrix[row - 1][col].name === 'dirt') {
+
+                            return (this.withProbaOf(0.5) ? this.tiles.rock : this.tiles.stone);
 
                         } else if (this.withProbaOf(0.1) &&
                             !!this.matrix[row - 1] &&
                             !!this.matrix[row - 1][col] &&
-                            this.matrix[row - 1][col].name === 'dirt') {
+                              this.matrix[row - 1][col].name === 'dirt') {
 
                             return this.tiles.tree;
                         }
@@ -171,31 +143,69 @@
                     return null;
             }
         }
+
+        // add trees and grass
+        polishItUp() {
+            for (let row = 0; row < this.numOfRows; row++) {
+                if (!!this.matrix[row + 1]) {
+                    for (let col = 0; col < this.numOfCols; col++) {
+
+                        if (!!this.matrix[row][col] &&
+                             !this.hasTileAbove(row, col) &&
+                              this.matrix[row][col].name === 'dirt') {
+
+                            this.matrix[row][col] = this.tiles.grass;
+
+                        } else if (!!this.matrix[row][col] &&
+                                    !this.hasTileAbove(row, col) &&
+                                     this.matrix[row][col].name === 'tree') {
+                                     
+                            this.injectTree(row, col);
+                        }
+                    }
+                }
+            }
+        }
+
+        injectTree(row, col) {
+            for (let i = 0; i < 3; i++) {
+                // Trunk
+                this.matrix[row + i][col]         = this.tiles.tree;
+                // Leaves
+                this.matrix[row + 3 + i][col - 1] = this.tiles.leaf;
+                this.matrix[row + 3 + i][col]     = this.tiles.leaf;
+                this.matrix[row + 3 + i][col + 1] = this.tiles.leaf;
+            }
+        }
+
+        injectCloud(row, col) {
+
+        }
     }
 
     class TileGridUI {
         constructor(tileGridInstance, gameSessionInstance, tileSize) {
-            this.grid = tileGridInstance;
-            this.session = gameSessionInstance;
-            this.numOfRows = tileGridInstance.matrix.length;
-            this.numOfCols = tileGridInstance.matrix[0].length;
-            this.width = tileSize * this.numOfCols;
-            this.$node = this.createGridNode();
-            this.$tileContainer = {};
-            this.$countTiles = {};
+            this.grid           = tileGridInstance;
+            this.session        = gameSessionInstance;
+            this.numOfRows      = tileGridInstance.matrix.length;
+            this.numOfCols      = tileGridInstance.matrix[0].length;
+            this.width          = tileSize * this.numOfCols;
+            this.$node          = this.createGridNode();
+            this.$tileContainer = new Object();
+            this.$countTiles    = new Object();
         }
 
         createGridNode() {
             const $node = $('<div />');
-            $node.attr('id', 'game-grid')
-                .css('width', this.width + 'px');
+                  $node.attr('id', 'game-grid')
+                       .css('width', this.width + 'px');
 
             return ($node);
         }
 
         createTileNode(url) {
             const $tileNode = $('<div />');
-            $tileNode.css('background-image', `url(${url})`);
+                  $tileNode.css('background-image', `url(${url})`);
 
             return ($tileNode);
         }
@@ -207,7 +217,7 @@
 
         renderGrid($parentNode) {
             $parentNode.css('background', 'none')
-                .html(this.$node);
+                       .html(this.$node);
 
             for (let row = 0; row < this.numOfRows; row++) {
                 for (let col = 0; col < this.numOfCols; col++) {
@@ -216,22 +226,20 @@
                     this.$node.prepend($tile);
 
                     this.grid.onChange(row, col, (newTile) => {
-                        if (!newTile) {
-                            $tile.css('background-image', '');
-                        }
-                        else $tile.css('background-image', `url( ${newTile.imgPath} )`);
+                        !newTile ? $tile.css('background-image', '') : $tile.css('background-image', `url( ${newTile.imgPath} )`);
                     });
 
                     $tile.on('click', function () {
-                        if (this.session.activeElement instanceof Tool && !!this.grid.matrix[row][col] &&
-                            this.session.activeElement.tileType === this.grid.matrix[row][col].name) {
+                        if (this.session.activeElement instanceof Tool && 
+                          !!this.grid.matrix[row][col] &&
+                            this.session.activeElement.tileTypes.includes(this.grid.matrix[row][col].name)) {
 
-                            this.grid.setTile(row, col, '');
                             this.updateCounter(
-                                this.$countTiles[this.session.activeElement.tileType],
-                                this.$tileContainer[this.session.activeElement.tileType],
-                                ++this.session.count[this.session.activeElement.tileType]
+                                  this.$countTiles   [this.grid.matrix[row][col].name],
+                                  this.$tileContainer[this.grid.matrix[row][col].name],
+                                ++this.session.count [this.grid.matrix[row][col].name]
                             );
+                            this.grid.setTile(row, col, '');
                             return;
                         }
 
@@ -240,65 +248,73 @@
 
                             this.grid.setTile(row, col, this.session.activeElement);
                             this.updateCounter(
-                                this.$countTiles[this.session.activeElement.name],
-                                this.$tileContainer[this.session.activeElement.name],
+                                  this.$countTiles[this.session.activeElement.name],
+                                  this.$tileContainer[this.session.activeElement.name],
                                 --this.session.count[this.session.activeElement.name]
                             );
                             return;
                         }
                     }.bind(this));
-
                 }
             }
         }
 
         renderToolkit($parentNodeTools, $parentNodeTiles, $parentNodeGrid) {
             for (const tool in this.session.tools) {
-                const $toolContainer = $('<div />').attr({
+                const $toolContainer = $( '<div />' ).attr({
                     'id': tool,
                     'class': 'toolkit-element',
                     'data-type': this.session.tools[tool].types,
                 });
-                $toolContainer.append($('<div />').css({
+
+                $toolContainer.append($( '<div />' )
+                              .css({
                     'background': 'url(' + this.session.tools[tool].imgPath + ')',
                     'background-size': 'contain',
                 }));
-                $toolContainer.append($('<p />').html(this.session.tools[tool].name));
-                $parentNodeTools.append($toolContainer);
+
+                $toolContainer.append($( '<p />' )
+                              .html(this.session.tools[tool].name));
 
                 $toolContainer.on('click', function () {
                     this.session.activeElement = this.session.tools[tool];
                     $parentNodeGrid.css('cursor', 'url(' + this.session.activeElement.cursorImgPath + '), auto');
-                    $('.toolkit-element.active').toggleClass('active');
+                    $( '.toolkit-element.active' ).toggleClass('active');
                     $toolContainer.toggleClass('active');
                 }.bind(this));
+
+                $parentNodeTools.append($toolContainer);
             }
             for (const tile in this.session.tiles) {
-                this.$tileContainer[tile] = $('<div />').attr({
+                this.$tileContainer[tile] = $( '<div />' ).attr({
                     'id': tile,
                     'class': 'toolkit-element empty',
                     'data-type': this.session.tiles[tile].types,
                 });
-                this.$countTiles[tile] = $('<div />').css({
+
+                this.$countTiles[tile] = $( '<div />' ).css({
                     'background': 'url(' + this.session.tiles[tile].imgPath + ')',
                     'background-size': 'contain',
                 }).attr('id', 'count-' + tile)
-                    .html(this.session.count[tile])
-                    .appendTo(this.$tileContainer[tile]);
-                this.$tileContainer[tile].append($('<p />').html(this.session.tiles[tile].name));
-                $parentNodeTiles.append(this.$tileContainer[tile]);
+                  .html(this.session.count[tile])
+                  .appendTo(this.$tileContainer[tile]);
+
+                this.$tileContainer[tile].append($( '<p />' ).html(this.session.tiles[tile].name));
 
                 this.$tileContainer[tile].on('click', function () {
                     this.session.activeElement = this.session.tiles[tile];
                     $parentNodeGrid.css('cursor', 'url(' + this.session.activeElement.cursorImgPath + '), auto');
-                    $('.toolkit-element.active').toggleClass('active');
+                    $( '.toolkit-element.active' ).toggleClass('active');
                     this.$tileContainer[tile].toggleClass('active');
                 }.bind(this));
+
+                $parentNodeTiles.append(this.$tileContainer[tile]);
             }
         }
 
         updateCounter($countTileser, $countTileserParent, count) {
             $countTileser.html(count);
+
             if (count === 0) $countTileserParent.addClass('empty');
             if (count === 1) $countTileserParent.removeClass('empty');
         }
--- js/game.js
+++ js/game.js
@@ -6,62 +6,66 @@
 // Namespace and main variables for our instance of the game
 const Minecraft = {
     html: {
-        $gameContainer: $('#game-container'),
-        $toolkitToolsContainer: $('#tools-container'),
-        $toolkitTilesContainer: $('#tiles-container'),
-        $startBtn: $('.start-game'),        
+        $toolkitToolsContainer: $( '#tools-container' ),
+        $toolkitTilesContainer: $( '#tiles-container' ),
+        $gameContainer:         $( '#game-container' ),
+        $startBtn:              $( '.start-game' ),        
     },
+
     tools: {
-        axe: createTool('axe', 'tree'),
-        bucket: createTool('bucket', 'lava'),
-        pickaxe: createTool('pickaxe', 'rock'),
-        shovel: createTool('shovel', 'dirt'),
+        pickaxe: createTool('pickaxe', ['rock', 'stone', 'gate', 'diamond']),
+        shovel:  createTool('shovel',  ['dirt', 'grass']),
+        axe:     createTool('axe',     ['tree', 'leaf']),
+        bucket:  createTool('bucket',  ['lava']),
     },
+
     tiles: {
         diamond: createTile('diamond'),
-        dirt: createTile('dirt'),
-        gate: createTile('gate'),
-        grass: createTile('grass'),
-        lava: createTile('lava'),
-        leaf: createTile('leaf'),
-        rock: createTile('rock'),
-        stone: createTile('stone'),
-        tnt: createTile('tnt'),
-        tree: createTile('tree'),
+        grass:   createTile('grass'),
+        stone:   createTile('stone'),
+        dirt:    createTile('dirt'),
+        gate:    createTile('gate'),
+        lava:    createTile('lava'),
+        leaf:    createTile('leaf'),
+        rock:    createTile('rock'),
+        tree:    createTile('tree'),
+        tnt:     createTile('tnt'),
     },
-    numOfRows: undefined,
-    numOfCols: 200,
-    tileSize: 50,   // in px
-    tileGrid: undefined,
+
+    tileGrid:   undefined,
     tileGridUI: undefined,
+    numOfRows:  undefined,
+    numOfCols:  100,
+    tileSize:   50,   // in px
 }
 
 // Initiates the game page
-Minecraft.init = function () {
+Minecraft.init      = function () {
     this.html.$startBtn.one('click', this.startGame.bind(this));
 };
 
 // Event handler
 Minecraft.startGame = function () {
 
-    this.numOfRows = Math.floor(this.html.$gameContainer.height() / this.tileSize)
-    const gridMatrix = build2dArray(this.numOfRows, this.numOfCols);
+    this.numOfRows        = Math.floor($( window ).height() / this.tileSize)
+    const gridMatrix      = build2dArray(this.numOfRows, this.numOfCols);
     const callbacksMatrix = build2dArray(this.numOfRows, this.numOfCols);
 
-    this.session = createGameSession(this.tools, this.tiles);
-    this.tileGrid = createTileGrid(gridMatrix, callbacksMatrix, this.tiles);
-    this.tileGridUI = createTileGridUI(this.tileGrid, this.session, this.tileSize);
+    this.session    = createGameSession(this.tools, this.tiles);
+    this.tileGrid   = createTileGrid   (gridMatrix, callbacksMatrix, this.tiles);
+    this.tileGridUI = createTileGridUI (this.tileGrid, this.session, this.tileSize);
+
     this.tileGridUI.render(this.html.$gameContainer, this.html.$toolkitToolsContainer, this.html.$toolkitTilesContainer);
 };
 
 // --------------------------------------------------------------------------------------
 // General functions that may be reused outside this project
 function build2dArray(numOfRows, numOfCols) {
-    const matrix = new Array(numOfRows);
+    const matrix = new Array(numOfRows).fill(null);
 
     let i = -1;
     while (++i < numOfRows) {
-        matrix[i] = new Array(numOfCols);
+        matrix[i] = new Array(numOfCols).fill(null);
     }
     return matrix;
 }
