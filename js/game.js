b3ed4e24ea62bdb910ac5c294b82d95d94abebfe
Ilann0 <ilann.ohayon@gmail.com>
2019-12-07 19:19:19 +0200

    added choose size buttons to game page

--- js/game.js
+++ js/game.js
@@ -1,78 +1,109 @@
 'use strict';
 
-// MAKE SURE TO UNCOMMENT ON DEPLOY ####################################################
-// window.onload = function () {
+window.onload = function () {
 
-// Namespace and main variables for our instance of the game
-const Minecraft = {
-    html: {
-        $toolkitToolsContainer: $( '#tools-container' ),
-        $toolkitTilesContainer: $( '#tiles-container' ),
-        $gameContainer:         $( '#game-container' ),
-        $startBtn:              $( '.start-game' ),        
-    },
+    // Namespace and main variables for our instance of the game
 
-    tools: {
-        pickaxe: createTool('pickaxe', ['rock', 'stone', 'gate', 'diamond']),
-        shovel:  createTool('shovel',  ['dirt', 'grass']),
-        axe:     createTool('axe',     ['tree', 'leaf']),
-        bucket:  createTool('bucket',  ['lava']),
-    },
+    const Minecraft = {
+        html: {
+            $toolkitToolsContainer: $( '#tools-container' ),
+            $toolkitTilesContainer: $( '#tiles-container' ),
+            $gameContainer:         $( '#game-container' ),
+            $startBtn:              $( '.start-game' ),        
+        },
 
-    tiles: {
-        diamond: createTile('diamond'),
-        grass:   createTile('grass'),
-        stone:   createTile('stone'),
-        dirt:    createTile('dirt'),
-        gate:    createTile('gate'),
-        lava:    createTile('lava'),
-        leaf:    createTile('leaf'),
-        rock:    createTile('rock'),
-        tree:    createTile('tree'),
-        tnt:     createTile('tnt'),
-        cloud:   createTile('cloud'),
-    },
+        tools: {
+            pickaxe: createTool('pickaxe', ['rock', 'stone', 'gate', 'diamond']),
+            shovel:  createTool('shovel',  ['dirt', 'grass']),
+            axe:     createTool('axe',     ['tree', 'leaf']),
+            bucket:  createTool('bucket',  ['lava']),
+        },
 
-    tileGrid:   undefined,
-    tileGridUI: undefined,
-    numOfRows:  undefined,
-    numOfCols:  200,
-    tileSize:   50,   // in px
-}
+        tiles: {
+            diamond: createTile('diamond'),
+            grass:   createTile('grass'),
+            stone:   createTile('stone'),
+            dirt:    createTile('dirt'),
+            gate:    createTile('gate'),
+            lava:    createTile('lava'),
+            leaf:    createTile('leaf'),
+            rock:    createTile('rock'),
+            tree:    createTile('tree'),
+            tnt:     createTile('tnt'),
+            cloud:   createTile('cloud'),
+        },
 
-// Initiates the game page
-Minecraft.init      = function () {
-    this.html.$startBtn.one('click', this.startGame.bind(this));
-};
-
-// Event handler
-Minecraft.startGame = function () {
-
-    this.numOfRows        = Math.floor($( window ).height() / this.tileSize) > 8 ? Math.floor($( window ).height() / this.tileSize) : 8;
-    const gridMatrix      = build2dArray(this.numOfRows, this.numOfCols);
-    const callbacksMatrix = build2dArray(this.numOfRows, this.numOfCols);
-
-    this.session    = createGameSession(this.tools, this.tiles);
-    this.tileGrid   = createTileGrid   (gridMatrix, callbacksMatrix, this.tiles);
-    this.tileGridUI = createTileGridUI (this.tileGrid, this.session, this.tileSize);
-
-    this.tileGridUI.render(this.html.$gameContainer, this.html.$toolkitToolsContainer, this.html.$toolkitTilesContainer);
-};
-
-// --------------------------------------------------------------------------------------
-// General functions that may be reused outside this project
-function build2dArray(numOfRows, numOfCols) {
-    const matrix = new Array(numOfRows).fill(null);
-
-    let i = -1;
-    while (++i < numOfRows) {
-        matrix[i] = new Array(numOfCols).fill(null);
+        tileGrid:   undefined,
+        tileGridUI: undefined,
+        numOfRows:  undefined,
+        numOfCols:  100,
+        tileSize:   50,   // in px
     }
-    return matrix;
-}
 
-// --------------------------------------------------------------------------------------
-// And this is where all the magic happens
-Minecraft.init();
+    // Initiates the game page
+    Minecraft.init      = function () {
+        this.html.$startBtn.one('click', this.startGame.bind(this));
+    };
+
+    // Event handler
+    Minecraft.startGame = function () {
+
+        this.numOfRows        = Math.floor($( window ).height() / this.tileSize) > 8 ? Math.floor($( window ).height() / this.tileSize) : 8;
+        const gridMatrix      = build2dArray(this.numOfRows, this.numOfCols);
+        const callbacksMatrix = build2dArray(this.numOfRows, this.numOfCols);
+
+        this.session    = createGameSession(this.tools, this.tiles);
+        this.tileGrid   = createTileGrid   (gridMatrix, callbacksMatrix, this.tiles);
+        this.tileGridUI = createTileGridUI (this.tileGrid, this.session, this.tileSize);
+
+        this.tileGridUI.render(this.html.$gameContainer, this.html.$toolkitToolsContainer, this.html.$toolkitTilesContainer);
+    };
+
+    const $sizeButtons = $( '.select-size > button' );
+
+    $sizeButtons.click(function() {
+        $sizeButtons.removeClass('active');
+        $( this ).addClass('active');
+
+        switch ( this.id ) {
+            case 'small':
+                Minecraft.numOfCols = 50;
+                break;
+
+            case 'medium':
+                Minecraft.numOfCols = 100;
+                break;
+
+            case 'big':
+                Minecraft.numOfCols = 300;
+                break;
+
+            case 'custom':
+                let num;
+
+                while( isNaN(num) || num === '' ) {
+                    num = prompt('How wide do you want your world to be?\n(in number of blocks)');
+                }
+                Minecraft.numOfCols = parseInt(num);
+                break;
+        }
+    });
+
+    // --------------------------------------------------------------------------------------
+    // General functions that may be reused outside this project
+    function build2dArray(numOfRows, numOfCols) {
+        const matrix = new Array(numOfRows).fill(null);
+
+        let i = -1;
+        while (++i < numOfRows) {
+            matrix[i] = new Array(numOfCols).fill(null);
+        }
+        return matrix;
+    }
+
+    // --------------------------------------------------------------------------------------
+    // And this is where all the magic happens
+    Minecraft.init();
+
 // ------------------------------------------------------------------------------------------
-// } // MAKE SURE TO UNCOMMENT ON DEPLOY ####################################################
+}
